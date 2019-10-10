5c71868e66158dfe5bf067b5572fbe21361490bb
Arthur Iasbeck <arthuriasbeck@gmail.com>
2019-10-10 10:19:18 -0300

    Melhorei a geração do gráfico f(x) e do ponto ótimo encontrado.

--- 1/secaoAurea/secaoAurea.m
+++ 1/secaoAurea/secaoAurea.m
@@ -6,8 +6,8 @@ b = 600;
 tal = 0.618;
 cp = 1e-8;
 
-alfa = a + (1 - tal)*(b - a);
-beta = a + tal*(b - a);
+alfa = a + (1 - tal)*(b - a)
+beta = a + tal*(b - a)
 fAlfa = f(alfa);
 fBeta = f(beta);
 
@@ -33,9 +33,15 @@ end
 xOpt = (alfa+beta)/2;
 fOpt = f(xOpt);
 
+fprintf('x* = %.4f\n', xOpt);
+fprintf('f(x*) = %.4f\n', fOpt);
+fprintf('k = %d\n', k);
+
 figure; x = linspace(0,600); plotI(x,f(x),'-'); hold on;
 plotI(xOpt, fOpt, 'o');
 titleI('$f(x)$');
 xlabelI('$x$');
 ylabelI('$f$');
 cropPlotI;
+axisComma;
+printI('secAur');
