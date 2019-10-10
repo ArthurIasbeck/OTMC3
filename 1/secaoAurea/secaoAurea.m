d18d7275740293dad3e491db23bf7c0d331cf886
Arthur Iasbeck <arthuriasbeck@gmail.com>
2019-10-10 09:49:34 -0300

    Implementei o Método da Seção Áurea e já realizei sua validação com a função objetivo que o professor deu em sala.

---
+++ 1/secaoAurea/secaoAurea.m
@@ -0,0 +1,41 @@
+init; 
+format long g
+
+a = 0;
+b = 600;
+tal = 0.618;
+cp = 1e-8;
+
+alfa = a + (1 - tal)*(b - a);
+beta = a + tal*(b - a);
+fAlfa = f(alfa);
+fBeta = f(beta);
+
+k = 1;
+
+while abs(a-b) > cp
+    if fBeta < fAlfa 
+        a = alfa;
+        alfa = beta;
+        fAlfa = fBeta;
+        beta = a + tal*(b - a);
+        fBeta = f(beta);
+    elseif fAlfa <= fBeta
+        b = beta;
+        beta = alfa;
+        fBeta = fAlfa;
+        alfa = a + (1 - tal)*(b - a);
+        fAlfa = f(alfa);
+    end
+    k = k + 1;
+end
+
+xOpt = (alfa+beta)/2;
+fOpt = f(xOpt);
+
+figure; x = linspace(0,600); plotI(x,f(x),'-'); hold on;
+plotI(xOpt, fOpt, 'o');
+titleI('$f(x)$');
+xlabelI('$x$');
+ylabelI('$f$');
+cropPlotI;
